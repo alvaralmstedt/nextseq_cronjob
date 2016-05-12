200e642d866a7106b523c53cda16cfd6d734db60
xalmal <xalmal@medair1.medair.lcl>
2016-05-12 16:24:07 +0200

    added cronjob stuff

--- cronjob.sh
+++ cronjob.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 
+DATE=$(date | sed 's/ /_/g' | sed 's/:/_/'g | cut -d"_" -f-6)
+OLD=$(readlink -f /tmp/nextseq_cronjob/NextSeq500175/*)
 
 
+NS75=/jumbo/Nextseq500175
+
+ls $NS75 > /tmp/nextseq_cronjob/lsNS75.${DATE}.tmp
+NEW=/tmp/nextseq_cronjob/lsNS75.${DATE}.tmp
+
+diff $OLD $NEW > /tmp/nextseq_cronjob/NextSeq500175/differences_$DATE
+
+DIFFERENCES=$(wc -l /tmp/nextseq_cronjob/NextSeq500175/differences_$DATE | cut -f 1 -d " ")
+
+if [ $DIFFERENCES > 0 ] ; then
+    rm $OLD
 #_______________
 
 
