ed2c622d6204304461c273c5ec9daaf4f68624be
xjmatm <xjmatm@medair1.medair.lcl>
2016-05-16 14:45:18 +0200

    starting to add loop to remove illegal chars

--- cronjob.sh
+++ cronjob.sh
@@ -27,19 +27,28 @@ then
 	SHEETCHECK=(ls /jumbo/Nextseq500175/${RUN} | grep -e "SampleSheet.csv)
         if [ !-z $SHEETCHECK ] ; 
 	then
-	
-		mkdir $RUN
-		RUNLOC=/jumbo/WorkingDir/Runs
-        	module load bcl2fastq/2.17.1.14
+		
 		#------------REMOVE ILLEGAL CHARACTERS FROM SAMPLESHEET---------->
+		
 		cp /jumbo/Nextseq500175/${RUN}/SampleSheet.csv ${TMP_LOC}/old.csv
 		DATALINE=$(cat -n ${TMP_LOC}/old.csv | grep -e "Data" | sed -r 's/ +/ /g' | cut -f1)
 		UPTO=$(($DATALINE-1))
 		sed -n "$DATALINE"',$p' ${TMP_LOC}/old.csv > ${TMP_LOC}/DATA_tmp
-		ILLEGALCHARS=$(echo " -?-(-)-[-]-/-\\-=-+-<->-:-;-\"-'-,-*-^-|-&-.")
-
-
+		ILLEGALCHARS=$(echo "?- -(-)-[-]-/-\\-=-+-<->-:-;-\"-'-,-*-^-|-&-.")
+		for k in $(seq 1 22);
+		do
+		CHAR=$(echo $ILLEGALCHARS | cut -f${k})
+		if [ "$CHAR" == "\" ] ;
+		then
+			CHAR=\\\\
+		fi
+		
 		head -n${UPTO} ${TMP_LOC}/old.csv > ${TMP_LOC}/old.csv
+            	
+		#-------------------RUN BCL2FASTQ AND FASTQC-------------------->
+		mkdir $RUN
+                RUNLOC=/jumbo/WorkingDir/Runs
+                module load bcl2fastq/2.17.1.14
 
 
 		nohup bcl2fastq  --runfolder-dir /jumbo/Nextseq500175/$RUN -o ${RUNLOC}/$RUN -r4 -p4 -d4 -w4 --barcode-mismatches 1 --no-lane-splitting --min-log-level TRACE > ${RUNLOC}/${RUN}/${RUN}_nohup.txt
