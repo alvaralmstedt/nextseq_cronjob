d3a29a0624513773acbc6b4bf91f6e06363c2057
xjmatm <xjmatm@medair1.medair.lcl>
2016-05-16 14:03:53 +0200

    Changed a whole lot of stuff

--- cronjob.sh
+++ cronjob.sh
@@ -9,38 +9,63 @@ NS75=/jumbo/Nextseq500175
 ls $NS75 > /jumbo/apps/misc-scripts/nextseq_cronjob/nextseq_500175_dirlist/lsNS75.${DATE}.tmp
 NEW=/jumbo/apps/misc-scripts/nextseq_cronjob/nextseq_500175_dirlist/lsNS75.${DATE}.tmp
 
-DIFF_FILE=/jumbo/apps/misc-scripts/nextseq_cronjob/tmp-files/nextseq_500175/differences_$DATE
-diff $OLD $NEW | grep ">" | cut -f 2 -d " " > $DIFF_FILE
-DIFFERENCES=$(wc -l $DIFF_FILE | cut -f 1 -d " ")
+TMP_LOC=/jumbo/apps/misc-scripts/nextseq_cronjob/tmp-files/nextseq_500175
+diff $OLD $NEW | grep ">" | cut -f 2 -d " " > ${TMP_LOC}/differences_$DATE
+DIFFERENCES=$(wc -l ${TMP_LOC}/differences_$DATE | cut -f 1 -d " ")
 
-if [ $DIFFERENCES > 0 ] ; then
+if [ $DIFFERENCES > 0 ] ; 
+then
 	rm $OLD
 	COUNTDIFF=1
 	cd /jumbo/WorkingDir/Runs
 	
-    for i in $(seq 1 $DIFFERENCES) ; do
+    	for i in $(seq 1 $DIFFERENCES) ; do
         RUN=$(sed "${COUNTDIFF}q;d" $DIFF_FILE)
-        mkdir $RUN
+        
+	#--------CHECK IF SAMPLESHEET EXISTS-------------->
 
-        module load bcl2fastq/2.17.1.14
+	SHEETCHECK=(ls /jumbo/Nextseq500175/${RUN} | grep -e "SampleSheet.csv)
+        if [ !-z $SHEETCHECK ] ; 
+	then
+	
+		mkdir $RUN
+		RUNLOC=/jumbo/WorkingDir/Runs
+        	module load bcl2fastq/2.17.1.14
+		#------------REMOVE ILLEGAL CHARACTERS FROM SAMPLESHEET---------->
+		cp /jumbo/Nextseq500175/${RUN}/SampleSheet.csv ${TMP_LOC}/old.csv
+		DATALINE=$(cat -n ${TMP_LOC}/old.csv | grep -e "Data" | sed -r 's/ +/ /g' | cut -f1)
+		UPTO=$(($DATALINE-1))
+		sed -n "$DATALINE"',$p' ${TMP_LOC}/old.csv > ${TMP_LOC}/DATA_tmp
+		ILLEGALCHARS=$(echo " -?-(-)-[-]-/-\\-=-+-<->-:-;-\"-'-,-*-^-|-&-.")
 
-        nohup bcl2fastq  --runfolder-dir /jumbo/Nextseq500175/$RUN -o /jumbo/WorkingDir/Runs/$RUN -r4 -p4 -d4 -w4 --barcode-mismatches 1 --no-lane-splitting --min-log-level TRACE > /jumbo/WorkingDir/Runs/${RUN}/${RUN}_nohup.txt
-        cp /jumbo/Nextseq500175/${RUN}/SampleSheet.csv /jumbo/WorkingDir/Runs/${RUN}/.
-        time /jumbo/WorkingDir/Programs/NextSeq/NS_FastqMergeQC_3.pl $RUN >> /jumbo/WorkingDir/Runs/${RUN}/${RUN}_nohup.txt
-        time /jumbo/WorkingDir/Programs/NextSeq/NS_createRunReport_3.pl MD $RUN >> /jumbo/WorkingDir/Runs/${RUN}_nohup.txt
 
-        COUNTDIFF=$(($COUNTDIFF+1))
-	INITALS=$(grep -e "Investigator Name," /jumbo/WorkingDir/Runs/${RUN}/SampleSheet.csv | cut -f2 -d",")
-        EXPERIMENT_NAME=$(grep -e "Experiment Name," /jumbo/WorkingDir/Runs/${RUN}/SampleSheet.csv | cut -f2 -d",")
-        EMAIL_ADDRESS=$(grep -e "${INITIALS}|" /jumbo/apps/misc-scripts/nextseq_cronjob/investigators/investigators.txt | cut -d"|" -f2)
-        INVESTIGATOR_NAME=$(grep -e "${INITIALS}|" /jumbo/apps/misc-scripts/nextseq_cronjob/investigators/investigators.txt | cut -d"|" -f3)
-        EMAIL=$"""From: \"NextSeq500175\" <NextSeq500175.noreply@medair.sahlgrenska.gu.se>
+		head -n${UPTO} ${TMP_LOC}/old.csv > ${TMP_LOC}/old.csv
+
+
+		nohup bcl2fastq  --runfolder-dir /jumbo/Nextseq500175/$RUN -o ${RUNLOC}/$RUN -r4 -p4 -d4 -w4 --barcode-mismatches 1 --no-lane-splitting --min-log-level TRACE > ${RUNLOC}/${RUN}/${RUN}_nohup.txt
+        	cp ${TMP_LOC}/SampleSheet.csv ${RUNLOC}/${RUN}/.
+        	time /jumbo/WorkingDir/Programs/NextSeq/NS_FastqMergeQC_3.pl $RUN >> ${RUNLOC}/${RUN}/${RUN}_nohup.txt
+        	time /jumbo/WorkingDir/Programs/NextSeq/NS_createRunReport_3.pl MD $RUN >> ${RUNLOC}/${RUN}_nohup.txt
+
+		MAILNOTE=$(echo "Find data and fastqc-report at: ${RUNLOC}/${RUN}")
+
+        	COUNTDIFF=$(($COUNTDIFF+1))
+		INITALS=$(grep -e "Investigator Name," ${RUNLOC}/${RUN}/SampleSheet.csv | cut -f2 -d",")
+        	EXPERIMENT_NAME=$(grep -e "Experiment Name," ${RUNLOC}/${RUN}/SampleSheet.csv | cut -f2 -d",")
+        	EMAIL_ADDRESS=$(grep -e "${INITIALS}|" /jumbo/apps/misc-scripts/nextseq_cronjob/investigators/investigators.txt | cut -d"|" -f2)
+        	INVESTIGATOR_NAME=$(grep -e "${INITIALS}|" /jumbo/apps/misc-scripts/nextseq_cronjob/investigators/investigators.txt | cut -d"|" -f3)
+	else
+            	EMAIL_ADDRESS=$(grep -e "ADMIN|" /jumbo/apps/misc-scripts/nextseq_cronjob/investigators/investigators.txt | cut -d"|" -f2)
+                INVESTIGATOR_NAME=$(grep -e "ADMIN|" /jumbo/apps/misc-scripts/nextseq_cronjob/investigators/investigators.txt | cut -d"|" -f3)
+		MAILNOTE=$(echo "Error: Bcl2fastq and FastQC did not run - no SampleSheet.csv in $RUN")
+	fi
+	EMAIL=$"""From: \"NextSeq500175\" <NextSeq500175.noreply@medair.sahlgrenska.gu.se>
 To: \"$INVESTIGATOR_NAME\" <$EMAIL_ADDRESS>
 Subject: Your Sequencing job $EXPERIMENT_NAME has finished!
 MIME-Version: 1.0
 Content-Type: text/plain
 
-Find data and fastqc-report at: ${RUNLOC}/${RUN}
+$MAILNOTE
 
 $EXPERIMENT_NAME finished at `date`
 """
@@ -49,5 +74,5 @@ $EXPERIMENT_NAME finished at `date`
 
 done
 fi
-rm $DIFF_FILE
+rm ${TMP_LOC}/differences_$DATE
 
