030ae0c0bc2a8c7d1f6b6e0f2c2098e343fbf892
Alvar Almstedt <alvar.almstedt@gmail.com>
2016-05-17 13:30:30 +0200

    Added err function and comments

--- cronjob.sh
+++ cronjob.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -l
 
+# Print some informative error meassages
+err() {
+        echo "$1 exited unexpectedly";
+            exit 1;
+}
+
 # Function for checking the exit code of a child process
 checkExit() {
 if [ "$1" == "0" ]; then
@@ -9,36 +15,43 @@ else
 fi
 }
 
+#Format the date output
 DATE=$(date | sed 's/ /_/g' | sed 's/:/_/'g | cut -d"_" -f-6)
 
+#Check status of base directory
 OLD=$(readlink -f /jumbo/apps/misc-scripts/nextseq_cronjob/nextseq_500175_dirlist/*)
 
 NS75=/jumbo/Nextseq500175
 
+#Create temporary file list
 ls $NS75 > /jumbo/apps/misc-scripts/nextseq_cronjob/nextseq_500175_dirlist/lsNS75.${DATE}.tmp
-checkexit $? "ls1"
+checkExit $? "ls1"
 
+#New file list
 NEW=/jumbo/apps/misc-scripts/nextseq_cronjob/nextseq_500175_dirlist/lsNS75.${DATE}.tmp
 
 TMP_LOC=/jumbo/apps/misc-scripts/nextseq_cronjob/tmp-files/nextseq_500175
 
+#Check differences between new and old file list as save
 diff $OLD $NEW | grep ">" | cut -f 2 -d " " > ${TMP_LOC}/differences_$DATE
-checkexit $? "diff1"
+checkExit $? "diff1"
 
 DIFFERENCES=$(wc -l ${TMP_LOC}/differences_$DATE | cut -f 1 -d " ")
 
+#If there are any changes, enter this if statement
 if [ $DIFFERENCES > 0 ] ; 
 then
-	rm $OLD
-    checkexit $? "rm1"
+	#Remove old filelist
+    rm $OLD
+    checkExit $? "rm1"
 	COUNTDIFF=1
 	cd /jumbo/WorkingDir/Runs
-	
+	    #For every new directory in the new filelist
     	for i in $(seq 1 $DIFFERENCES) ; do
         RUN=$(sed "${COUNTDIFF}q;d" ${TMP_LOC}/differences_$DATE)
         
 	    #--------CHECK IF SAMPLESHEET EXISTS-------------->
-
+            
 	        SHEETCHECK=$(ls /jumbo/Nextseq500175/${RUN} | grep -e "SampleSheet.csv")
 
             if [ ! -z "$SHEETCHECK" ] ; 
@@ -46,18 +59,24 @@ then
 		
 		    #------------REMOVE ILLEGAL CHARACTERS FROM SAMPLESHEET---------->
             
+            #Copy the samplesheet to be modified
             cp /jumbo/Nextseq500175/${RUN}/SampleSheet.csv ${TMP_LOC}/old.csv
-            checkexit $? "cp samplesheet1"
-
+            checkExit $? "cp samplesheet1"
+           
+            #Format space characters
             DATALINE=$(cat -n ${TMP_LOC}/old.csv | grep -e "Data" | sed -r 's/ +/ /g' | cut -f1)
             
+            #Skip column descriptors
             UPTO=$(($DATALINE+1))
             
+            #Parse out only lines under [Data] from sample sheet
             sed -n "$UPTO"',$p' ${TMP_LOC}/old.csv > ${TMP_LOC}/DATA_tmp
-            checkexit $? "sed datafield"
-
+            checkExit $? "sed datafield"
+            
+            #List illegal characters
             ILLEGALCHARS=$(echo "?- -(-)-\[-]-\/-\\-=-+-<->-:-;-\"-'-*-\^-|-&-\.")
             
+            #Loop over each illegal character
             for k in $(seq 1 21);
             do
             
@@ -66,54 +85,73 @@ then
                 then
                     CHAR=\\\\
                 fi
-                checkexit $? "illegalcharacters"
+                checkExit $? "illegalcharacters"
                 
+                #Replace illegal character with underscore
                 sed -i "s/${CHAR}/_/g" ${TMP_LOC}/DATA_tmp
-                checkexit $? "sed underscores"
+                checkExit $? "sed underscores"
 		done
 		
+        #Put unmodified lines from old shamples sheet to new
         head -n${DATALINE} ${TMP_LOC}/old.csv > ${TMP_LOC}/SampleSheet.csv
-        checkexit $? "head old csv"
-
+        checkExit $? "head old csv"
+        
+        #Append modified lines to new sample sheet
         cat ${TMP_LOC}/DATA_tmp >> ${TMP_LOC}/SampleSheet.csv
-		checkexit $? "cat DATA_tmp"
-
+		checkExit $? "cat DATA_tmp"
+        
+        #Replace old sample sheet with new
         cp ${TMP_LOC}/SampleSheet.csv /jumbo/Nextseq500175/${RUN}/.
-        checkexit $? "copy samplesheet2"
+        checkExit $? "copy samplesheet2"
 
 		#-------------------RUN BCL2FASTQ AND FASTQC-------------------->
 
 		mkdir $RUN
         RUNLOC=/jumbo/WorkingDir/Runs
         module load bcl2fastq/2.17.1.14
-        checkexit $? "module load"
-
+        checkExit $? "module load"
+        
+        #Run bcl2fastq
 		nohup bcl2fastq  --runfolder-dir /jumbo/Nextseq500175/$RUN -o ${RUNLOC}/$RUN -r4 -p4 -d4 -w4 --barcode-mismatches 1 --no-lane-splitting --min-log-level TRACE > ${RUNLOC}/${RUN}/${RUN}_nohup.txt
-		checkexit $? ""
-
+		checkExit $? ""
+        
+        #Move sample sheet to run location
         cp /jumbo/Nextseq500175/${RUN}/SampleSheet.csv ${RUNLOC}/${RUN}/.
-		checkexit $? "cp samplesheet"
+		checkExit $? "cp samplesheet"
 
+        #Run NS_FastqMergeQC_3.pl
         time /jumbo/WorkingDir/Programs/NextSeq/NS_FastqMergeQC_3.pl $RUN
-        checkexit $? "NS_FastMergeQC_3.pl"
+        checkExit $? "NS_FastMergeQC_3.pl"
 
+        #Run NS_createRunReport_3.pl
         time /jumbo/WorkingDir/Programs/NextSeq/NS_createRunReport_3.pl MD $RUN
-        checkexit $? "NS_createRunReport_3.pl"
+        checkExit $? "NS_createRunReport_3.pl"
 
+        #Save location of resultfiles to string
 		MAILNOTE=$(echo "Find data and fastqc-report at: ${RUNLOC}/${RUN}")
 
         COUNTDIFF=$(($COUNTDIFF+1))
-		INITALS=$(grep -e "Investigator Name," ${RUNLOC}/${RUN}/SampleSheet.csv | cut -f2 -d",")
+		
+        #Save initials of the Investogator to string
+        INITALS=$(grep -e "Investigator Name," ${RUNLOC}/${RUN}/SampleSheet.csv | cut -f2 -d",")
+        
+        #Save experiment name to string
         EXPERIMENT_NAME=$(grep -e "Experiment Name," ${RUNLOC}/${RUN}/SampleSheet.csv | cut -f2 -d",")
+        
+        #Fetch email address from file containing list of initials, email adresses and names
         EMAIL_ADDRESS=$(grep -e "${INITIALS}|" /jumbo/apps/misc-scripts/nextseq_cronjob/investigators/investigators.txt | cut -d"|" -f2)
+        
+        #Fetch Investigator name from file containing list of initials, email addresses and names
         INVESTIGATOR_NAME=$(grep -e "${INITIALS}|" /jumbo/apps/misc-scripts/nextseq_cronjob/investigators/investigators.txt | cut -d"|" -f3)
-        checkexit $? "grep1"
+        checkExit $? "grep1"
 	else
+        #If no sample sheet is found, these errors will be emailed instead
         EMAIL_ADDRESS=$(grep -e "ADMIN|" /jumbo/apps/misc-scripts/nextseq_cronjob/investigators/investigators.txt | cut -d"|" -f2)
         INVESTIGATOR_NAME=$(grep -e "ADMIN|" /jumbo/apps/misc-scripts/nextseq_cronjob/investigators/investigators.txt | cut -d"|" -f3)
 		MAILNOTE=$(echo "Error: Bcl2fastq and FastQC did not run - no SampleSheet.csv in ${RUN}")
-        checkexit $? "grep2"
+        checkExit $? "grep2"
 	fi
+        #Email to be sent
 	    EMAIL=$"""From: \"NextSeq500175\" <NextSeq500175.noreply@medair.sahlgrenska.gu.se>
 To: \"$INVESTIGATOR_NAME\" <$EMAIL_ADDRESS>
 Subject: Your Sequencing job $EXPERIMENT_NAME has finished!
@@ -124,10 +162,12 @@ $MAILNOTE
 
 $EXPERIMENT_NAME finished at `date`
 """
-
+    #Send email
     echo "$EMAIL" | /usr/sbin/sendmail -i -t
-    checkexit $? "sendmail"
+    checkExit $? "sendmail"
         done
 fi
+
+#Remove differences file
 rm ${TMP_LOC}/differences_$DATE
-checkexit $? "rm_differences"
+checkExit $? "rm_differences"
