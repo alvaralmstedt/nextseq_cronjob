fb37cbacaa9541000da74018f9b6790c70e88c03
xjmatm <xjmatm@medair1.medair.lcl>
2016-05-18 11:14:58 +0200

    added module load java

--- cronjob.sh
+++ cronjob.sh
@@ -101,7 +101,7 @@ then
 		checkExit $? "cat DATA_tmp"
         
         #Replace old sample sheet with new
-        cp ${TMP_LOC}/SampleSheet.csv /jumbo/Nextseq500175/${RUN}/.
+        cp ${TMP_LOC}/SampleSheet.csv /jumbo/Nextseq500175/${RUN}/
         checkExit $? "copy samplesheet2"
 
 		#-------------------RUN BCL2FASTQ AND FASTQC-------------------->
@@ -109,7 +109,8 @@ then
 		mkdir $RUN
         RUNLOC=/jumbo/WorkingDir/Runs
         module load bcl2fastq/2.17.1.14
-        checkExit $? "module load"
+        module load java
+	checkExit $? "module load"
         
         #Run bcl2fastq
 		nohup bcl2fastq  --runfolder-dir /jumbo/Nextseq500175/$RUN -o ${RUNLOC}/$RUN -r4 -p4 -d4 -w4 --barcode-mismatches 1 --no-lane-splitting --min-log-level TRACE > ${RUNLOC}/${RUN}/${RUN}_nohup.txt
